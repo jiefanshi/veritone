55a39ff3a46ba423d05a43eaa78f775ed8601e9a
wfei <wfei@powercosts.com>
2021-10-13 16:11:52 -0500

    Fisrt Commit Test

---
+++ src/com/veritone/interview/main/BinarySearchTree.java
@@ -0,0 +1,101 @@
+package com.veritone.interview.main;
+
+import java.lang.annotation.Target;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        List<Integer> array = new ArrayList<>();
+        array.add(12);
+        array.add(11);
+        array.add(90);
+        array.add(82);
+        array.add(7);
+        array.add(9);
+        BinarySearchTree test1 = new BinarySearchTree();
+        TreeNode root = test1.arrayToBinarySearchTree(array);
+        test1.printTreeNodes(root);
+    }
+
+    public void printTreeNodes(TreeNode root) {
+        if (root ==null) {
+            return;
+        }
+        System.out.println(root.value);
+        printTreeNodes(root.left);
+        printTreeNodes(root.right);
+    }
+
+    private int deepestNodeDepth = 0;
+    private TreeNode deepestNode = null;
+
+    /**
+     * Get the deepestNode in Binary Search Tree
+     */
+    public TreeNode getDeepestNode() {
+        return deepestNode;
+    }
+
+    /**
+     * Get the deepth of the Binary Search Tree
+     */
+    public int getDeepestNodeDepth() {
+        return deepestNodeDepth;
+    }
+
+    /**
+     * Convert an unsorted array to Binary Search Tree
+     * @param array input array
+     */
+    public TreeNode arrayToBinarySearchTree (List<Integer> array) {
+        if (array.isEmpty()) {
+            return null;
+        }
+        TreeNode root = new TreeNode(array.get(0));
+        for (Integer value : array) {
+            root = insert(root, value);
+        }
+        return root;
+    }
+
+    /**
+     * Insert a data in the Binary Search Tree
+     * @param root create a TreeNode
+     * @param target target number
+     */
+    public TreeNode insert(TreeNode root, int target) {
+        if (root == null) {
+            return new TreeNode(target);
+        }
+        insertHelper(root, target, 1);
+        return root;
+    }
+
+    /**
+     * Insert a data in the Binary Search Tree Helper
+     * @param root insert to root
+     * @param target target to be inserted
+     */
+    public void insertHelper(TreeNode root, int target, int currentDepth) {
+        if (root == null) {
+            return;
+        }
+        if (root.value == target) {
+            if (currentDepth > deepestNodeDepth) {
+                deepestNodeDepth = currentDepth;
+                deepestNode = root;
+            }
+        } else if (root.value > target) {
+            if (root.left == null) {
+                root.left = new TreeNode(target);
+            }
+            insertHelper(root.left, target, currentDepth + 1);
+        } else {
+            if (root.right == null) {
+                root.right = new TreeNode(target);
+            }
+            insertHelper(root.right, target, currentDepth + 1);
+        }
+    }
+}
---
+++ src/com/veritone/interview/test/BinarySearchTreeTest.java
@@ -0,0 +1,107 @@
+package com.veritone.interview.test;
+
+
+import com.veritone.interview.main.BinarySearchTree;
+import com.veritone.interview.main.TreeNode;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class BinarySearchTreeTest {
+
+    BinarySearchTree test;
+    List<Integer> array;
+
+    @BeforeEach
+    public void setUp() {
+        test = new BinarySearchTree();
+        array = new ArrayList<>();
+    }
+
+    @Test
+    @DisplayName("BSTConstruction.arrayToTree empty test")
+    public void testArrayToBinarySearchTreeEmpty () {
+        TreeNode root = test.arrayToBinarySearchTree(array);
+        assertNull(root);
+    }
+
+    @Test
+    @DisplayName("BSTConstruction.arrayToTree test")
+    public void testArrayToBinarySearchTree () {
+        array.add(12);
+        array.add(11);
+        array.add(90);
+        array.add(82);
+        array.add(7);
+        array.add(9);
+        TreeNode root = test.arrayToBinarySearchTree(array);
+
+        assertEquals(12, root.value);
+        assertEquals(11, root.left.value);
+        assertEquals(7, root.left.left.value);
+        assertEquals(9, root.left.left.right.value);
+        assertEquals(90, root.right.value);
+        assertEquals(82,root.right.left.value);
+    }
+
+    @Test
+    @DisplayName("BSTConstruction.arrayToTree duplication test")
+    public void testArrayToBinarySearchTreeDuplication () {
+        array.add(12);
+        array.add(11);
+        array.add(90);
+        array.add(82);
+        array.add(7);
+        array.add(9);
+        array.add(9);
+        array.add(12);
+        array.add(90);
+        TreeNode root = test.arrayToBinarySearchTree(array);
+
+        assertEquals(12, root.value);
+        assertEquals(11, root.left.value);
+        assertEquals(7, root.left.left.value);
+        assertEquals(9, root.left.left.right.value);
+        assertNull(root.left.left.right.left);
+        assertNull(root.left.left.right.right);
+        assertEquals(90, root.right.value);
+        assertEquals(82,root.right.left.value);
+    }
+
+    @Test
+    @DisplayName("BSTConstruction.getDeepestNodeDepth test")
+    public void testGetDeepestNodeDepth () {
+        array.add(12);
+        array.add(11);
+        array.add(90);
+        array.add(82);
+        array.add(7);
+        array.add(9);
+        TreeNode root = test.arrayToBinarySearchTree(array);
+        assertEquals(4, test.getDeepestNodeDepth());
+    }
+
+    @Test
+    @DisplayName("BSTConstruction.getDeepestNode test")
+    public void testGetDeepestNode () {
+        array.add(12);
+        array.add(11);
+        array.add(90);
+        array.add(82);
+        array.add(7);
+        array.add(9);
+        TreeNode root = test.arrayToBinarySearchTree(array);
+        assertEquals(9, test.getDeepestNode().value);
+    }
+
+    @AfterAll
+    public static void cleanUp(){
+        System.out.println("Tests finish.");
+    }
+
+
+}
